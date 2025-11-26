c9252e8d6f9e4382a2aba4d8de0391405ac62fec
Na'Sir Miller <nmiller1329@gmail.com>
2025-11-25 19:43:51 -0500

    attempting to perform anal

--- empowered/test.sql
+++ empowered/test.sql
@@ -1,5 +1,9 @@
 USE empowered;
 GO
 
-SELECT *
-FROM fullCensusEstimate;
+-- SELECT *
+-- FROM fullCensusEstimate;
+
+-- SELECT COUNT(*) AS [Num Estimates], COUNT(DISTINCT variable_id) AS [Num Variables], COUNT(DISTINCT state_fips) AS [Num States]  FROM CensusEstimate;
+
+SELECT DISTINCT ce.state_fips, cs.state_name FROM CensusEstimate ce INNER JOIN CensusState cs ON ce.state_fips=cs.state_fips;
